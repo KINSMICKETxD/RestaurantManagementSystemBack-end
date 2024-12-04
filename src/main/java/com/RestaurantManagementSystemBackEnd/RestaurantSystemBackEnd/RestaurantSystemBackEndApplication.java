3c3989259244075ca923214ccc5157590ab53a79
KINSMICKETxD <aboudaorg@gmail.com>
2024-12-04 12:49:05 +0100

    initial commit

---
+++ src/main/java/com/RestaurantManagementSystemBackEnd/RestaurantSystemBackEnd/RestaurantSystemBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.RestaurantManagementSystemBackEnd.RestaurantSystemBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantSystemBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantSystemBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RestaurantManagementSystemBackEnd/RestaurantSystemBackEnd/RestaurantSystemBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RestaurantManagementSystemBackEnd.RestaurantSystemBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantSystemBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
